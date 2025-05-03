3a5dc7cd94ccb6a0eade5b3b346e104a62b987ba
Lluc Vivet Torras <lluc@vivet.cat>
2025-05-03 19:59:53 +0200

    Get, put,  post, delete
    aaaaaa

--- backend/skyscanner_api/main.py
+++ backend/skyscanner_api/main.py
@@ -2,28 +2,89 @@ from fastapi import FastAPI, HTTPException
 from api.flights_indicative import search_flights
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from typing import Optional
+from fastapi import status
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional, List
+
+
 app = FastAPI()
+
+# Load routers
+
+class Travel(BaseModel):
+      username: str
+      destination: Optional[list[str]] = []
+      origin: str
+      language: list[str]
+      cabin_class: str
+      disponibility: list[str]
+      max_price: int
+      hire_car: bool
+      hotel: bool
+      preferences: Optional[list[str]] = []
+
+travel_list = [Travel(username="Julliz",destination=[],origin="Perú",language=["spanish"], cabin_class="business",disponibility=["2025-06-10", "2025-06-17"], max_price=2500, hire_car=True, hotel=True, preferences=["beach", "underrated locations"]),
+              Travel(username="Oggy",destination=[],origin="Nigeria",language=["Arabe"], cabin_class="business",disponibility=["2025-06-10", "2025-07-1"], max_price=10000, hire_car=False, hotel=True, preferences=["mountain", "nightlife", "culture and art"]),
+              Travel(username="Lolo",destination=[],origin="Londres",language=["english"], cabin_class="business",disponibility=["2025-04-10", "2025-07-20"], max_price=1500, hire_car=False, hotel=False, preferences=["old cities", "culture and art"]),]
+
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to HackUPC2025!"}
 
-@app.get("/flights/origin/{origin}/destination/{destination}")
-def get_flights(origin: str, destination: str):
-    """
-    Endpoint to get indicative flight prices.
-    """
-    # Load environment variables
-    load_dotenv()
-    api_key = os.getenv("SKYSCANNER_API_KEY")
+@app.get("/travel/")
+async def users():
+    return travel_list
+
+@app.post("/travel/", response_model=Travel, status_code=201)
+async def travel(travel: Travel):
+    if type(search_username(travel.username)) == Travel:
+        raise HTTPException(status_code=404, detail="El usuario ya existe")
+ 
+    if travel.destination is None:
+        travel.destination = []
+    if travel.preferences is None:
+        travel.preferences = []
+        
+    travel_list.append(travel)
+    return travel
+
+@app.put("/travel/{username}", status_code=202)
+async def travel(travel: Travel):
+
+    found = False
+
+    for index,saved_travel in enumerate(travel_list):
+        if saved_travel.username == travel.username:
+            travel_list[index] = travel
+            found = True
+
+    if not found:
+        raise HTTPException(status_code=404, detail="El usuario no existe")
+
+    return travel
+
+@app.delete("/travel/{username}", status_code=204)
+async def travel(username: str):
+
+    found = False
+
+
+    for index,saved_travel in enumerate(travel_list):
+        if saved_travel.username == username:
+            del travel_list[index]
+            found = True
+    if not found:
+        raise HTTPException(status_code=404, detail="El usuario no existe")
+
     
-    if not api_key:
-        raise HTTPException(status_code=400, detail="API key not found in environment variables.")
+def search_username(username: str):
+    travelers = filter(lambda travel: travel.username == username, travel_list)
+    try:
+        return list(travelers)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
     
-    # Call the search_flights function
-    result = search_flights(api_key, origin, destination)
-    
-    if result is None:
-        raise HTTPException(status_code=400, detail="Failed to fetch flight data.")
-    
-    return result
