9bfef53614ad8bfd0cf357bdab0983051e707139
Big-Lolo <95545807+Big-Lolo@users.noreply.github.com>
2025-05-04 03:59:02 +0200

    Interval

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 import Home from './pages/Home';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+
+    };
+
+    fetchData();
+    const interval = setInterval(fetchData, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <Home />
